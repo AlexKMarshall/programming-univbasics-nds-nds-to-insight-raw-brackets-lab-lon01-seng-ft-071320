933f2d6e64dd78c1876c2d3eb1e249a33f55f7f9
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-21 18:31:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,14 +3,7 @@ require 'directors_database'
 require 'pp'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  pp nds
+  result = {}
   
   nds.count.times do |director_index|
     director = nds[director_index][:name]
@@ -22,13 +15,4 @@ def directors_totals(nds)
     result[director] = profit
   end
   result
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all thes
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
 end
